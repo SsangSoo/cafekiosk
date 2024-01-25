97853577a77374d14061f9a079d8d7643197fc4f
SsangSoo <kss10080@naver.com>
2024-01-26 00:28:08 +0900

    Controller Testing~

--- src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -1,6 +1,8 @@
 package sample.cafekiosk.spring.api.controller.product.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
+import sample.cafekiosk.spring.domain.Product;
 import sample.cafekiosk.spring.domain.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.ProductType;
 
@@ -12,4 +14,21 @@ public class ProductCreateRequest {
     private String name;
     private int price;
 
+    @Builder
+    private ProductCreateRequest(ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product toEntity(String nextProductNumber) {
+        return Product.builder()
+                .productNumber(nextProductNumber)
+                .type(type)
+                .sellingStatus(sellingStatus)
+                .name(name)
+                .price(price)
+                .build();
+    }
 }
